355ca4688fe1c79ce6a00d2a3afb4200fdb92de0
Paul Haddad <paul.haddad@yello.co>
2017-06-16 16:54:25 -0500

    You different keywords

--- sms_receiving_service.rb
+++ sms_receiving_service.rb
@@ -7,9 +7,15 @@ end
 
 post '/' do
   from_number = params[:From]
-  body = params[:Body]
+  body = params[:Body].downcase
   client = TwilioServiceWrapper.new
   service = client.view_service("MG9edb0847b6258296a9023225ac64f667")
-  client.send_message(message_service: service, to_number: from_number, body: "Here you go!")
-  "Received a text from #{from_number} with #{body}"
+  "You sent me a text from #{from_number} with #{body}"
+  if body.match(/hello/)
+    client.send_message(message_service: service, to_number: from_number, body: "You said hello!")
+  elsif body.match(/good/)
+    client.send_message(message_service: service, to_number: from_number, body: "You said goodbye!")
+  else
+    client.send_message(message_service: service, to_number: from_number, body: "You didn't say anything!")
+  end
 end
